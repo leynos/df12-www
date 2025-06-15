431ccc8a2c91f8f3dd3d97bf7e1b79e0155683cc
Leynos <leynos@troubledskies.net>
2025-06-15 01:57:29 +0100

    Update GoDaddy provider info (#2)
    * Update GoDaddy provider info
    
    * Specify GoDaddy provider registry

---
+++ modules/static_site/tests/basic/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    godaddy-dns = {
+      source  = "registry.terraform.io/veksh/godaddy-dns"
+      version = ">= 0.3.12"
+    }
+  }
+}
+
+module "static_site" {
+  source = "../"
+
+  domain_name        = "www.example.com"
+  root_domain        = "example.com"
+  log_retention_days = 30
+
+  providers = {
+    aws         = aws
+    aws.useast1 = aws.useast1
+    godaddy-dns = godaddy-dns
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+provider "aws" {
+  alias  = "useast1"
+  region = "us-east-1"
+}
+
+provider "godaddy-dns" {
+  api_key    = "dummy"
+  api_secret = "dummy"
+}
